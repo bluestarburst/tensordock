17d96c281d450e6cdb33da63fc6ee8da6d4d9ed1
BlueStarBurst <hargreaves.bryant@gmail.com>
2024-09-26 23:24:14 -0500

    server

--- test/server.py
+++ test/server.py
@@ -8,11 +8,13 @@ from jupyter_client.multikernelmanager import MultiKernelManager
 class JupyterWebSocketServer:
     def __init__(self):
         self.kernel_manager = KernelManager()
-        # self.kernel_manager.start_kernel()
         self.clients = set()
+        self.kernel_manager.start_kernel()
+        
 
     async def handle_client(self, websocket, path):
         self.clients.add(websocket)
+        client_id = id(websocket)
         try:
             async for message in websocket:
                 data = json.loads(message)
@@ -23,19 +25,22 @@ class JupyterWebSocketServer:
                 elif data['action'] == 'execute_code':
                     # kernel_id = data['kernel_id']
                     code = data['code']
-                    result = await self.execute_code(code)
+                    cell_id = data['cell_id']
+                    result = await self.execute_code(code, cell_id)
                     await self.broadcast({'action': 'execution_result', 'result': result})
                 elif data['action'] == 'restart_kernel':
                     self.kernel_manager.restart_kernel()
                     print("Restarted kernel")
                     await self.broadcast({'action': 'kernel_restarted'})
                 elif data['action'] == 'canvas_data':
-                    await self.broadcast({'action': 'canvas_data', 'data': data['data']})
+                    tmp = data['data']
+                    tmp['id'] = str(client_id)
+                    await self.broadcast({'action': 'canvas_data', 'data': tmp}, client_id)
                     
         finally:
             self.clients.remove(websocket)
 
-    async def execute_code(self, code):
+    async def execute_code(self, code, cell_id):
         client = self.kernel_manager.client()
         msg_id = client.execute(code)
         result = {'outputs': []}
@@ -87,11 +92,11 @@ class JupyterWebSocketServer:
 
         return result
 
-    async def broadcast(self, message):
+    async def broadcast(self, message, client_id=None):
         if self.clients:
             # await asyncio.wait([client.send(json.dumps(message)) for client in self.clients])
             # TypeError: Passing coroutines is forbidden, use tasks explicitly.
-            await asyncio.gather(*[client.send(json.dumps(message)) for client in self.clients])
+            await asyncio.gather(*[client.send(json.dumps(message)) for client in self.clients if id(client) != client_id])
 
 
 async def main():
