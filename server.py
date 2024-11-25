4e65eee0553dbe3ef604a4f2f6e4a74100763058
BlueStarBurst <hargreaves.bryant@gmail.com>
2024-11-25 00:08:48 -0600

    open button exists?

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ print("bash test")
 turn_server_address = os.environ.get('TURN_ADDRESS', f"0.0.0.0:{os.environ.get('VAST_UDP_PORT_70001')}?transport=udp")
 turn_client_address = os.environ.get('TURN_ADDRESS', f"{os.environ.get('PUBLIC_IPADDR')}:{os.environ.get('VAST_UDP_PORT_70001')}?transport=udp")
 turn_username = os.environ.get('TURN_USERNAME', 'user')
-turn_password = os.environ.get('TURN_PASSWORD', os.environ.get('OPEN_BUTTON_TOKEN', 'password'))
+turn_password = os.environ.get('TURN_PASSWORD', 'password')
 
 logging.basicConfig(
     level=logging.DEBUG,
